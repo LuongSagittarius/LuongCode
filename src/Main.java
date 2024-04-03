bb5ff3f386f8dcb60cbf5da3bac8e1c2ae16451f
LuongSagittarius <ngoducluong.2004@gmail.com>
2024-04-03 14:53:44 +0700

    quan ly khach san

--- src/Main.java
+++ src/Main.java
@@ -4,24 +4,27 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("1. đăng nhập");
-        System.out.println("2. đăng ký");
-        System.out.println("0. thoát chương trình");
-            Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            System.out.println("1. đăng nhập");
+            System.out.println("2. đăng ký");
+            System.out.println("0. thoát chương trình");
+
             int choice = sc.nextInt();
-            switch (choice) {
-                case 1: {
-                    UserMain.login();
-                }
-                case 2: {
-                    UserMain.register();
-                }
-                case 0: {
-                    System.exit(0);
-                }
-                default: {
-                    System.out.println("Không hợp lệ");
-                }
+        switch (choice) {
+            case 1: {
+                UserMain.login();
             }
+            case 2: {
+                UserMain.register();
+            }
+            case 0: {
+                System.exit(0);
+            }
+            default: {
+                System.out.println("Không hợp lệ");
+            }
+        }
+        }
     }
 }
