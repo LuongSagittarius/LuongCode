583e09ea8164994e6d7cc4e8a443f185191f8541
LuongSagittarius <ngoducluong.2004@gmail.com>
2024-04-03 14:38:08 +0700

    quan ly khach san

--- src/Main.java
+++ src/Main.java
@@ -5,21 +5,16 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("1. đăng nhập");
+        System.out.println("2. đăng ký");
+        System.out.println("0. thoát chương trình");
             Scanner sc = new Scanner(System.in);
             int choice = sc.nextInt();
             switch (choice) {
                 case 1: {
-                    RoomMain.addNewRoom();
-                    break;
-                }
-                case 2: {
-
-                    break;
-                }
-                case 3: {
                     UserMain.login();
                 }
-                case 4: {
+                case 2: {
                     UserMain.register();
                 }
                 case 0: {
